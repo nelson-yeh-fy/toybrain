0299e0306e8d5a8a09d68aab42f0827bfd2fb59c
lindon3513 <lindon3513@gmail.com>
2016-11-27 16:51:29 +0800

    Update reactComponent.js

--- docker-nginx/html/reactComponents.js
+++ docker-nginx/html/reactComponents.js
@@ -1,11 +1,7 @@
  /**
   * React v15.4.1
-  */
-  
-/*React.render(  
-			React.DOM.h1(null, "Hello world! ReactJS!"),  
-			document.getElementById("app") 
-		); */
+  */ 
+
 function formatName(userName){
   return userName.firstName + "&" + userName.LastName;
 };
@@ -25,14 +21,24 @@ ReactDOM.render(
   //<h1>Hello, world!</h1>,
   element,
   document.getElementById('root')
-);		
+);    
 
 class StoryBox extends React.Component {
-	render(){
-		return( <div>Story Box </div> );
-	}
+  render(){
+    const now = new Date();
+    const topics = ['Html', 'JavaScript', 'JSX', 'ReactJS'];
+    return( 
+      <div>
+        <h1>Story Box</h1>
+        <h3>Current time: {now.toDateString()} {now.toTimeString()}</h3>
+        <ul>
+          {topics.map(topic => <li>{topic}</li>)}
+        </ul>
+      </div> 
+    );
+  }
 }
-ReactDOM.render(  	
-  	<StoryBox />,
-  	document.getElementById('story-app')
+ReactDOM.render(    
+    <StoryBox />,
+    document.getElementById('story-app')
 );
