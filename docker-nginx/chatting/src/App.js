8bd094c972226ea476616c1bd7dab93b806fb9e4
lindon3513Tpe <lindon3513@gmail.com>
2017-05-05 14:40:19 +0800

    Make it React-Redux application

--- docker-nginx/chatting/src/App.js
+++ docker-nginx/chatting/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Cart from './components/cart';
+import Todo from './components/todo';
+
 class App extends Component {
   render() {
     return (
@@ -11,90 +14,14 @@ class App extends Component {
           <h2>Welcome to dockerized board</h2>
         </div>
         <div className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <MessageBoard />
+          <Todo />
+        </div>
+        <div className="App-intro">
+          <Cart />
         </div>
       </div>
     );
   }
 }
-
-class MessageBoard extends Component {
-  constructor() {
-    super();
-    this.state = {
-      showComments: true,
-      items: [{text:"Dispatching unit 0310, 0311 to CFS2017-00123", key:"0123456879"}]
-    };
-    this._handleSubmit = this._handleSubmit.bind(this);
-  }
-
-  render() {
-    return (
-      <div className="App-MsgListMain">
-        <div className="header">
-          <form onSubmit={this._handleSubmit}>
-            <input ref={(a) => this._inputElement = a} placeholder="Enter task">
-            </input>
-            <button type="submit">Add</button>
-          </form>
-        </div>
-        <TodoItems entries={this.state.items} />
-      </div>
-    );
-  }
-
-  _handleSubmit(e) {
-    e.preventDefault(); //prevent page reload when click the submit button
-    let itemArray = this.state.items;
-    itemArray.push(
-      {
-        text: this._inputElement.value,
-        key: Date.now()
-      }
-    );
-    this.setState({
-      items: itemArray
-    });
-    this._inputElement.value = "";    
-  }
-}
-
-class TodoItems extends Component {
-  render() {
-    let todoEntries = this.props.entries;
-    function createTasks(item) {
-      return <li key={item.key}>{item.text}</li>
-    }
-    let listItems = todoEntries.map(createTasks);
-
-    return (
-      <div>
-        <ul className="theList">
-          {listItems}
-        </ul>
-      </div>
-    );
-  }
-}
-
-// var TodoItems = React.createClass({
-//   render: function () {
-//     var todoEntries = this.props.entries;
-
-//     function createTasks(item) {
-//       return <li key={item.key}>{item.text}</li>
-//     }
-
-//     var listItems = todoEntries.map(createTasks);
-
-//     return (
-//       <ul className="theList">
-//         {listItems}
-//       </ul>
-//     );
-//   }
-// });
-
 
 export default App;
--- docker-nginx/chatting/src/index.js
+++ docker-nginx/chatting/src/index.js
@@ -1,9 +1,18 @@
+//React
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+//Redux
+import { Provider } from 'react-redux';
+import Store from './store';
+
+const StoreInstance = Store(); //Initiate a new redux store
+
 ReactDOM.render(
-  <App />,
+  <Provider store={StoreInstance}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
