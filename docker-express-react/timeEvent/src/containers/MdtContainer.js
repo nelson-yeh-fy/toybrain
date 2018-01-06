18ed1a33238ccdd2b937add1da0e9b7831f392f2
lindon3513 <lindon3513@gmail.com>
2018-01-06 17:41:36 -0500

    add mdt container

---
+++ docker-express-react/timeEvent/src/containers/MdtContainer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as mdtActionCreators from '../actions/mdt';
+
+import MdtControlBar from '../components/mdtControlBar';
+import MdtCfsBrief from '../components/mdtCfsBrief';
+import MdtCfsInfo from '../components/mdtCfsInfo';
+
+class MdtContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div>
+            <MdtControlBar />
+            <MdtCfsBrief />
+            <MdtCfsInfo />
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        timeEvents: state.timeEvents, // timeEvents means TimeEventList's prop, state.timeEvent means rootReducer.timeEvent
+        mdtEvent: state.mdtshow,
+    };
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(mdtActionCreators, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MdtContainer);
