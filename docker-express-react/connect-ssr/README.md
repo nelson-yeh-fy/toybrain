5a19af01ace2a5557c24bb047a594df16e20da1a
Nelson Yeh <nyeh@CSITECH.COM>
2019-05-24 12:23:19 -0400

    Webpack and babel dependencies

---
+++ docker-express-react/connect-ssr/README.md
@@ -0,0 +1,1 @@
+This project was bootstrapped with http://www.react.express/babel
--- docker-express-react/connect-ssr/webpack.config.js
+++ docker-express-react/connect-ssr/webpack.config.js
@@ -4,5 +4,21 @@ module.exports = options => {
     output: {
       filename: 'bundle.js',
     },
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
   }
 }
